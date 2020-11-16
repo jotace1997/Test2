bf0527ef3108ffd2d4b62c4c0c631c5afe9c34bd
Juan Carlos <sanchezcarajuancarlos@gmail.com>
2020-11-16 13:40:04 +0100

    Inicio del proyecto

---
+++ include/matrizCSR.h
@@ -0,0 +1,25 @@
+#ifndef _MATRIZ_CSR_H
+#define _MATRIZ_CSR_H
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class MatrizCSR
+{
+    private:
+        int filas;
+        int columnas;
+        int noNulos;
+
+        vector<long double> valores;
+        vector<int> idColumnas;
+        vector<int> ptrFilas;
+
+    public:
+        MatrizCSR();
+        ~MatrizCSR();
+        MatrizCSR(int f, int c, int nn);
+        MatrizCSR(const MatrizCSR &m);
+};
+#endif
